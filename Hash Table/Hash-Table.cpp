60e36746ee529c580a6672d6653bfa333c21d5af
EHranitel <kulko01@mail.ru>
2021-02-26 22:32:15 +0300

    1-2 homework

---
+++ Hash Table/Hash-Table.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include "Hash-Table.hpp"
+
+void createAndFillFileWithHashTable(std::string name, int* tableLens)
+{
+    std::ofstream file(name);
+
+    for (int i = 0; i < 26; i++)
+    {
+        file << (tableLens[i]) << std::endl;
+    }
+    
+    file.close();  
+}
+
+int main()
+{
+    hashTable table0(0);
+    hashTable table1(1);
+    hashTable table2(2);
+
+    table0.fillFromFile("Text.txt");
+    table1.fillFromFile("Text.txt");
+    table2.fillFromFile("Text.txt");
+
+    createAndFillFileWithHashTable("standartHash", table0.get());
+    createAndFillFileWithHashTable("hash1", table1.get());
+    createAndFillFileWithHashTable("hash2", table2.get());
+}   
---
+++ Hash Table/Hash-Table.hpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <array>
+#include <string>
+#include <list>
+#include <fstream>
+
+class hashContainer
+{
+    public:
+        int hash(std::string word, int currentHash)
+        {
+            if (currentHash == 1)
+            {
+                return hash1(word);
+            }
+
+            if (currentHash == 2)
+            {
+                return hash2(word);
+            }
+
+            return standartHash(word);
+        }
+
+        int standartHash(std::string word)
+        {
+            return abs(std::hash<std::string>{}(word));
+        }
+
+        int hash1(std::string word)
+        {
+            return (word[0] + word[word.length() - 1]) / 2;
+        }
+
+        int hash2(std::string word)
+        {
+            int hash = word[0] * 997;
+
+            int len = word.length();
+
+            for (int i = 1; i < len; i++)
+            {
+                hash = ((hash + word[i]) * 997) % 26;
+            }
+
+            return hash;
+        }
+};
+
+class hashTable
+{
+    private:
+        std::array<std::list<std::string>, 26> table;
+        int tableLens[26];
+        
+        hashContainer container;
+
+        int currentHash = 0;
+
+    public:
+        hashTable(int hashNumber)
+        {
+            currentHash = hashNumber;   
+        }
+
+        int* get()
+        {          
+            for (int i = 0; i < 26; i++)
+            {
+                tableLens[i] = table[i].size();
+            }
+
+            return tableLens;
+        }        
+
+        void add(std::string word)
+        {
+            table[container.hash(word, currentHash) % 26].push_front(word);
+        }
+
+        void fillFromFile(const char* Name)
+        {
+            std::string line;
+        
+            std::ifstream in(Name); 
+
+            if (in.is_open())
+            {
+                while (getline(in, line))
+                {
+                    int lineLen = line.length();
+
+                    int strLen = 0;
+
+                    for (int i = 0; i < lineLen; i++)
+                    {
+                        if (strLen > 0 && (line[i] < 65 || (line[i] < 97 && line[i] > 90) || line[i] > 122))
+                        {
+                            std::string word = line.substr (i - strLen, strLen);
+                            add(word);
+
+                            strLen = 0;
+                        }
+
+                        else
+                        {
+                            strLen++;
+                        }
+                    }
+                }
+            }
+
+            in.close();    
+        }
+
+        ~hashTable(){}
+};
+ 
